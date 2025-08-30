0e3f3d2e46b81fd7044faca50f6da076ed447847
Sean Mealey <smealey167@gmail.com>
2025-08-29 20:47:47 -0400

    corrected home row mods

--- keyboards/sofle/keymaps/illusionist899/keymap.c
+++ keyboards/sofle/keymaps/illusionist899/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
   KC_GRV,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_DEL,
-  KC_TAB,   KC_A,   KC_S,    KC_D,    LSFT(KC_F),    LCTL(KC_G),         RCTL(KC_H),    RSFT(KC_J),    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
+  KC_TAB,   KC_A,   KC_S,    KC_D,    LSFT_T(KC_F),    LCTL_T(KC_G),         RCTL_T(KC_H),    RSFT_T(KC_J),    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     KC_MEDIA_NEXT_TRACK,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
                  KC_LGUI,KC_LALT,KC_LCTL, TL_LOWR, LT(_NUMPAD, KC_ENT),      LT(_NUMPAD, KC_SPC),  TL_UPPR, KC_RCTL, KC_RALT, KC_APP
 ),
